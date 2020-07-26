0cec71da6cbad170b95c7c63ca4bc71e2ff752c8
dayoung-lee <dimmaculatey@gmail.com>
2020-07-26 18:36:14 +0900

    modify img src

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function Home() {
       <h2>짱구</h2>
       <p>짱구네 오신것을 환영합니다.</p>{' '}
       <img
-        src="https://lh3.googleusercontent.com/proxy/htFuElswEr3KDR8KSdrF-6kNNOrWbeg_TfeS7KDDjqhwN8aqWSpMcxikFuFlOP2AsdII2FpBIiVDa1knjhwNHvshzVBe7Y7ZsMV5AQuVlExj6LyrxRvZ07RNVpy2mIaZr2Pd1pETnUBRodDadvXpKCWXFWpIJvQizn7CWGczW0Vo1A"
+        src={require('./images/bodo_still06.jpg')}
         alt="짱구는 못말려 단체사진"
         style={{ width: '700px' }}
       ></img>
@@ -29,15 +29,13 @@ let contents = [
     id: 1,
     title: '이야기. 하나',
     description: '춤추는 짱구와 친구들',
-    imgSrc:
-      'https://file3.instiz.net/data/cached_img/upload/2018/02/28/2/722335bd9802de768767df9795096854.gif'
+    imgSrc: './images/722335bd9802de768767df9795096854.gif'
   },
   {
     id: 2,
     title: '이야기. 둘',
     description: '초코비 먹는 짱구',
-    imgSrc:
-      'https://lh3.googleusercontent.com/proxy/-fnzCi7iI9xssjp1qFH2neJ7E3EpXtHJwg2EONndngNc94muAyleAQQ5uVUwRGl9SZueh3J_wunNanUVyaoby9SbDzPAZMFox_5BxJJG0ay8euno6q7xkzcN6W-NM6Xdl5jJA-_yINj5j_cT2iv_n6RmvEdGJuJ_ZMAut87l7hPSiFgd'
+    imgSrc: './images/unnamed.gif'
   }
 ];
 
@@ -57,7 +55,7 @@ function Topic() {
   return (
     <div>
       <img
-        src={selected_topic.imgSrc}
+        src={require(`${selected_topic.imgSrc}`)}
         alt={selected_topic.description}
         style={{ width: '500px' }}
       ></img>
@@ -90,7 +88,7 @@ function Contact() {
       <h2>짱구네 집</h2>
       <p>주소 : 서울특별시 초록구 떡잎마을</p>
       <img
-        src="https://lh3.googleusercontent.com/proxy/UeCIGX5F4VffIW3cjTYfL7fTeBiBuKgFuqrNs8sRkFER2zmRCmDvdV6cU7GiQrqMsRaST66ZI80m3Dy6_z4fERdMZvd6xzCX57y7kVuzsuekDB00zFpwZklugeNq50zR2LAFLLmU0d9KasyNiBqv3ohKDX4p79inhmpzsvhZ7He9NQ"
+        src={require('./images/house.jpg')}
         alt="짱구네 집"
         style={{ width: '500px' }}
       ></img>
